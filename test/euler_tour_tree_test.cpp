73e4ce11a321939fcaba871a7bfcbcfcae0e18b4
Ethan DeTurk <ethan.deturk@stonybrook.edu>
2022-03-31 13:56:55 -0400

    Add test for strange root aggregate behavior

--- test/euler_tour_tree_test.cpp
+++ test/euler_tour_tree_test.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 
 #include <gtest/gtest.h>
 
@@ -98,6 +99,105 @@ TEST(EulerTourTreeSuite, stress_test) {
           << "Stress test validation failed, final state:"
           << std::endl
           << nodes;
+    }
+  }
+}
+
+TEST(EulerTourTreeSuite, random_links_and_cuts) {
+
+  // sketch variables
+  vec_t len = 10000;
+  vec_t err = 100;
+  //configure the sketch globally
+  Sketch::configure(len, err);
+
+  int nodecount = 10;
+  int n = 1;
+
+  //int seed = time(NULL);
+  int seed = 1648213336;
+  size_t space = Sketch::sketchSizeof();
+  void* sketch_space = malloc(space * nodecount);
+  std::vector<Sketch*> sketches;
+  // Update each sketch with an edge that easily identifies the node
+  for (int i = 0; i < nodecount; i++)
+  {
+    sketches.emplace_back(Sketch::makeSketch((char*)sketch_space + space*i, seed));
+    sketches[i]->update((vec_t)i);
+  }
+
+  std::vector<EulerTourTree> nodes;
+  nodes.reserve(nodecount);
+  for (int i = 0; i < nodecount; i++)
+  {
+    nodes.emplace_back(sketches[i], seed);
+  }
+
+  std::cout << "Seeding random links and cuts test with " << seed << std::endl;
+  srand(seed);
+  // Do random links and cuts
+  for (int i = 0; i < n; i++) {
+    int a = rand() % nodecount, b = rand() % nodecount;
+    if (rand() % 100 < 50) {
+      std::cout << "Link " << a << " to " << b << std::endl;
+      nodes[a].link(nodes[b]);
+    } else {
+      std::cout << "Cut " << a << " from " << b << std::endl;
+      nodes[a].cut(nodes[b]);
+    }
+    if (i % n/100 == 0)
+    {
+      ASSERT_TRUE(std::all_of(nodes.begin(), nodes.end(),
+            [](auto& node){return node.isvalid();}))
+          << "Stress test validation failed, final state:"
+          << std::endl
+          << nodes;
+    }
+  }
+
+  // Walk up from an occurrence of each node to the root of its auxiliary tre
+  std::unordered_map<SplayTreeNode*, Sketch*> aggs;
+  for (int i = 0; i < nodecount; i++)
+  {
+    SplayTreeNode *aux_root = nodes[i].edges.begin()->second.get();
+    while (aux_root->get_parent().get() != nullptr)
+    {
+      aux_root = aux_root->get_parent().get();
+    }
+    // Sanity checks
+    if (i == 3 || i == 5)
+    {
+      std::cout << "Node "
+                << i
+                << " aux tree root address: "
+                << aux_root
+                << " root sketch aggregate address: "
+                << aux_root->sketch_agg.get()
+                << std::endl;
+    }
+
+    if (i == 5) // CHANGE ME
+    {
+      std::pair<vec_t, SampleSketchRet> query = aux_root->sketch_agg.get()->query();
+      std::string sketch_ret;
+      if (query.second == SampleSketchRet::GOOD)
+      {
+        sketch_ret = "GOOD";
+      }
+      else if (query.second == SampleSketchRet::ZERO)
+      {
+        sketch_ret = "ZERO";
+      }
+      else if (query.second == SampleSketchRet::FAIL)
+      {
+        sketch_ret = "FAIL";
+      }
+      std::cout << "Node " << i << " value: " << query.first << " sketch ret: " << sketch_ret << std::endl;
+    }
+    SplayTreeNode *sentinel = SplayTree::get_last(nodes[i].edges.begin()->second).get();
+    if (aggs.find(sentinel) == aggs.end())
+    {
+      aggs[sentinel] = aux_root->sketch_agg.get();
     }
   }
 }
