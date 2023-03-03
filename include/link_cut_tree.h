92cbd38adaeb86af322487dd6063b080b45a4d9f
Quinten De Man <quinten.deman@gmail.com>
2023-03-03 16:15:57 -0500

    made some changes to the link cut tree interface

--- include/link_cut_tree.h
+++ include/link_cut_tree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "splay_tree.h"
+#include "types.h"
 
 class LinkCutTree;
 class SplayTree;
@@ -30,12 +31,20 @@ class LinkCutTree{
   std::vector<LinkCutNode*> roots;
   
   void access(const LinkCutNode& v);
-  void find_root(const LinkCutNode& v);
+  uint32_t path_aggregate(const LinkCutNode& v);
 
   public:
-    // links nodes v and w
-    bool link(const LinkCutNode& v, const LinkCutNode& w);
-    LinkCutNode* cut(const LinkCutNode& v);
-    uint32_t path_aggregate(const LinkCutNode& v);
+    LinkCutTree(node_id_t num_nodes);
+
+    // Given v a tree root and w a vertex in another tree, link the trees containing v and w by adding the edge(v, w), making w the parent of v
+    void link(node_id_t v, node_id_t w);
+    // Given node v not a tree root, divide the tree containing v into two trees by deleting the edge(v, parent(v))
+    void cut(node_id_t v);
+
+    node_id_t find_root(node_id_t v);
+
+    // Given node v return the edge(a, b) with the maximum or minimum weight on the path from the tree root to v
+    edge_id_t path_minimum(node_id_t v);
+    edge_id_t path_maximum(node_id_t v);
 };
 
