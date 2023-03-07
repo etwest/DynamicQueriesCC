46dfaecb3eb066a36450b479ab32e1dc28040528
Quinten (Lab) <quinten.deman@gmail.com>
2023-03-07 13:23:27 -0500

    redefined link cut tree interface to work on any input vertices

--- include/link_cut_tree.h
+++ include/link_cut_tree.h
@@ -7,42 +7,43 @@ class LinkCutTree;
 class SplayTree;
 
 class LinkCutNode{
+
   LinkCutNode* parent;
   LinkCutNode* dparent;
-
-  bool is_root;
-
-  std::vector<LinkCutNode*> children;
-  LinkCutNode* preferred_child;
+  LinkCutNode* left;
+  LinkCutNode* right;
   
+  //Each node contains the weight of the edge from itself to its parent in the represented tree  
   uint32_t edge_weight;
 
-
   public:
-    void set_edge_weight(uint32_t tier);
-
+    void splay();
+    void set_parent(LinkCutNode* parent);
+    void set_dparent(LinkCutNode* dparent);
+    void set_left(LinkCutNode* left);
+    void set_right(LinkCutNode* right);
+    void set_edge_weight(uint32_t weight);
 };
 
 class LinkCutTree{
-  //need path aggregate
-  //link
-  //cut
   
-  std::vector<LinkCutNode*> roots;
+  std::vector<LinkCutNode*> nodes;
   
-  void access(const LinkCutNode& v);
+  void expose(const LinkCutNode& v);
+  void evert(const LinkCutNode& v);
 
   public:
     LinkCutTree(node_id_t num_nodes);
+    ~LinkCutTree();
 
     // Given nodes v and w, link the trees containing v and w by adding the edge(v, w)
-    void link(node_id_t v, node_id_t w);
+    void link(node_id_t v, node_id_t w, uint32_t weight);
     // Given nodes v and w, divide the tree containing v and w by deleting the edge(v, w)
     void cut(node_id_t v, node_id_t w);
 
     node_id_t find_root(node_id_t v);
 
-    // Given node v return the edge(a, b) with the maximum weight on the path from the tree root to v and the weight itself
-    std::pair<edge_id_t, uint32_t> path_aggregate(node_id_t v);
+    // Given node v and w return the edge with the maximum weight on the path from v to w and the weight itself
+    std::pair<edge_id_t, uint32_t> path_aggregate(node_id_t v, node_id_t w);
 };
 
