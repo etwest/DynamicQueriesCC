e9022a0d2570623a82544583b72f9f917688a971
Daniel DeLayo <dandelayo@gmail.com>
2023-03-30 14:06:13 -0400

    binary graph stream reader

--- test/link_cut_tree_test.cpp
+++ test/link_cut_tree_test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include "link_cut_tree.h"
+#include "binary_graph_stream.h"
 
 static bool validate(LinkCutNode* v) {
     bool valid = true;
@@ -199,4 +200,9 @@ TEST(LinkCutTreeSuite, random_links_and_cuts) {
     for (auto agg : path_aggregates) {
         EXPECT_EQ(agg.second, agg.first->max) << "Aggregate incorrect" << std::endl;
     }
+}
+
+TEST(LinkCutTreeSuite, read_from_binary) {
+  BinaryGraphStream stream("kron_13_stream_binary", 100000);
+  stream.get_edge();
 }
