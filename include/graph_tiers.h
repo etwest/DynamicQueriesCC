dc5b05a4a1f19730309e02374bc9ff97059dc1a4
Quinten De Man <quinten.deman@gmail.com>
2023-06-03 13:28:09 -0400

    added MPI message sending/receiving in tier refresh function

--- include/graph_tiers.h
+++ include/graph_tiers.h
@@ -2,13 +2,12 @@
 
 #include "types.h"
 #include <vector>
+#include <mpi.h>
 
 #include "euler_tour_tree.h"
 #include "link_cut_tree.h"
 
 // Global variables for performance testing
-#ifndef TIME_VARIABLES
-#define TIME_VARIABLES
 extern long lct_time;
 extern long ett_time;
 extern long ett_find_root;
@@ -17,20 +16,21 @@ extern long sketch_query;
 extern long sketch_time;
 extern long refresh_time;
 extern long tiers_grown;
-#endif
 
-edge_id_t vertices_to_edge(node_id_t a, node_id_t b) {
+static edge_id_t vertices_to_edge(node_id_t a, node_id_t b) {
    return a<b ? (((edge_id_t)a)<<32) + ((edge_id_t)b) : (((edge_id_t)b)<<32) + ((edge_id_t)a);
 };
 
-enum EttOperationType{
+enum OperationType{
   EMPTY, LINK, CUT
 };
 
 typedef struct {
-  EttOperationType type = EMPTY;
-  edge_id_t edge = 0;
-} EttMessage;
+  OperationType type = EMPTY;
+  node_id_t endpoint1 = 0;
+  node_id_t endpoint2 = 0;
+  uint32_t start_tier = 0;
+} UpdateMessage;
 
 typedef struct {
   node_id_t endpoint1 = 0;
@@ -40,13 +40,9 @@ typedef struct {
 typedef struct {
   bool connected = false;
   edge_id_t cycle_edge = 0;
+  uint32_t weight = 0;
 } LctResponseMessage;
 
-typedef struct {
-  node_id_t endpoint1 = 0;
-  node_id_t endpoint2 = 0;
-  uint32_t weight = 0;
-} LctUpdateMessage;
 
 typedef struct {
   node_id_t v = 0;
@@ -55,14 +51,15 @@ typedef struct {
   edge_id_t query_result = 0;
 } RefreshMessage;
 
-class Tier {
-  node_id_t tier_number;
+class TierNode {
+  uint32_t tier_num;
+  uint32_t num_tiers;
   std::vector<EulerTourTree> ett_nodes;
   void update_tier(GraphUpdate update);
-  void ett_update_tier(EttMessage message);
+  void ett_update_tier(UpdateMessage message);
   void refresh_tier(RefreshMessage endpoint1, RefreshMessage endpoint2);
 public:
-  Tier(node_id_t num_nodes, node_id_t tier_number);
+  TierNode(node_id_t num_nodes, uint32_t tier_num, uint32_t num_tiers);
   std::vector<std::set<node_id_t>> get_cc();
 };
 
--- src/tier.cpp
+++ src/tier.cpp
@@ -1,7 +1,7 @@
 #include "../include/graph_tiers.h"
 
-Tier::Tier(node_id_t num_nodes, node_id_t tier_number) :
-    tier_number(tier_number) {
+TierNode::TierNode(node_id_t num_nodes, uint32_t tier_num, uint32_t num_tiers) :
+    tier_num(tier_num), num_tiers(num_tiers) {
     // Algorithm parameters
 	vec_t sketch_len = (num_nodes*num_nodes);
 	vec_t sketch_err = 10;
@@ -15,11 +15,11 @@ Tier::Tier(node_id_t num_nodes, node_id_t tier_number) :
     srand(seed);
     ett_nodes.reserve(num_nodes);
     for (node_id_t i = 0; i < num_nodes; ++i) {
-        ett_nodes.emplace_back(seed, i, tier_number);
+        ett_nodes.emplace_back(seed, i, tier_num);
     }
 }
 
-void Tier::update_tier(GraphUpdate update) {
+void TierNode::update_tier(GraphUpdate update) {
     edge_id_t edge = vertices_to_edge(update.edge.src, update.edge.dst);
     ett_nodes[update.edge.src].update_sketch((vec_t)edge);
     ett_nodes[update.edge.dst].update_sketch((vec_t)edge);
@@ -28,10 +28,10 @@ void Tier::update_tier(GraphUpdate update) {
     }
 }
 
-void Tier::ett_update_tier(EttMessage message) {
+void TierNode::ett_update_tier(UpdateMessage message) {
     if (message.type == EMPTY) return;
-    node_id_t a = (node_id_t)message.edge;
-	node_id_t b = (node_id_t)(message.edge>>32);
+    node_id_t a = message.endpoint1;
+	node_id_t b = message.endpoint2;
     if (message.type == LINK) {
         ett_nodes[a].link(ett_nodes[b]);
     } else {
@@ -39,47 +39,58 @@ void Tier::ett_update_tier(EttMessage message) {
     }
 }
 
-void Tier::refresh_tier(RefreshMessage endpoint1, RefreshMessage endpoint2) {
+void TierNode::refresh_tier(RefreshMessage endpoint1, RefreshMessage endpoint2) {
     for (RefreshMessage message: {endpoint1, endpoint2}) {
         // Check if the tree containing this endpoint is isolated
         uint32_t prev_tier_size = message.prev_tier_size;
         uint32_t this_tier_size = ett_nodes[message.v].get_size();
-        // Check for same size for isolated
         if (prev_tier_size != this_tier_size)
             continue;
 
         // Check for new edge to eliminate isolation
         if (message.query_result_type != GOOD)
             continue;
-
         node_id_t a = (node_id_t)message.query_result;
         node_id_t b = (node_id_t)(message.query_result>>32);
 
-        // Check if a path exists between the edge's endpoints
-        void* a_root = link_cut_tree.find_root(a);
-        void* b_root = link_cut_tree.find_root(b);
-        if (a_root == b_root) {
-            // Find the maximum tier edge on the path and what tier it first appeared on
-            std::pair<edge_id_t, uint32_t> max = link_cut_tree.path_aggregate(a,b);
-            node_id_t c = (node_id_t)max.first;
-            node_id_t d = (node_id_t)(max.first>>32);
+        // Query LCT node to check if this new edge forms a cycle
+        LctQueryMessage lct_query;
+        lct_query.endpoint1 = a;
+        lct_query.endpoint2 = b;
+        MPI_Send(&lct_query, sizeof(LctQueryMessage), MPI_BYTE, num_tiers+1, 0, MPI_COMM_WORLD);
 
-            // Remove the maximum tier edge on all paths where it exists
-            for (uint32_t i = max.second; i < ett_nodes.size(); i++) {
-                ett_nodes[i][c].cut(ett_nodes[i][d]);
-            }
-            link_cut_tree.cut(c,d);
+        LctResponseMessage lct_response;
+        MPI_Recv(&lct_response, sizeof(LctResponseMessage), MPI_BYTE, num_tiers+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // If there is a cycle formed, tell all necessary nodes to delete that edge
+        if (lct_response.connected) {
+            node_id_t c = (node_id_t)lct_response.cycle_edge;
+            node_id_t d = (node_id_t)(lct_response.cycle_edge>>32);
+
+            UpdateMessage cut_message;
+            cut_message.type = CUT;
+            cut_message.endpoint1 = c;
+            cut_message.endpoint2 = d;
+            cut_message.start_tier = lct_response.weight;
+            MPI_Bcast(&cut_message, sizeof(UpdateMessage), MPI_BYTE, tier_num, MPI_COMM_WORLD);
+
+            if (tier_num >= lct_response.weight)
+                ett_nodes[c].cut(ett_nodes[d]);
         }
 
-        // Join the ETTs for the endpoints of the edge on all tiers above the current
-        for (uint32_t i = tier+1; i < ett_nodes.size(); i++) {
-            ett_nodes[i][a].link(ett_nodes[i][b]);
-        }
-        link_cut_tree.link(a,b, tier+1);
+        // Tell all nodes above and including the current tier to add the new edge
+        UpdateMessage link_message;
+        link_message.type = LINK;
+        link_message.endpoint1 = a;
+        link_message.endpoint2 = b;
+        link_message.start_tier = tier_num;
+        MPI_Bcast(&link_message, sizeof(UpdateMessage), MPI_BYTE, tier_num, MPI_COMM_WORLD);
+
+        ett_nodes[a].link(ett_nodes[b]);
     }
 }
 
-std::vector<std::set<node_id_t>> Tier::get_cc() {
+std::vector<std::set<node_id_t>> TierNode::get_cc() {
 	std::vector<std::set<node_id_t>> cc;
 	std::set<EulerTourTree*> visited;
 	for (uint32_t i = 0; i < ett_nodes.size(); i++) {
