ca6d7fbbf883449e4e078101c68d5b8774a7117c
Evan West <etwest@cs.stonybrook.edu>
2025-02-04 14:51:03 -0500

    work in progress to allow for unit tests that run on a small graph

--- test/mpi_test_runner.cpp
+++ test/mpi_test_runner.cpp
@@ -2,25 +2,37 @@
 #include <gtest/gtest.h>
 #include "util.h"
 
+static const std::string fname = __FILE__;
+static size_t pos = fname.find_last_of("\\/");
+static const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
 
-std::string stream_file;
-int batch_size_arg;
-double height_factor_arg;
+std::string stream_file = curr_dir + "/test_streams/multiples_graph_1024_stream.data";
+int batch_size_arg = 4;
+double height_factor_arg = 1 / 10;
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
   
-  if (argc < 4) {
+  if (argc != 4 && argc != 1) {
     std::cerr << "INCORRECT NUMBER OF ARGUMENTS." << std::endl;
+    std::cerr << "Expect: stream_file, batch_size_arg, height_factor_arg" << std::endl;
+    std::cerr << "OR: Run without any arguments to use default values of all for unit testing" << std::endl;
     return EXIT_FAILURE;
   }
-
-  stream_file = argv[1];
-  batch_size_arg = atoi(argv[2]);
-  height_factor_arg = atof(argv[3]);
+  if (argc == 1) {
+    std::cerr << "RUNNING WITH DEFAULT ARGUMENTS" << std::endl;
+    std::cerr << "stream_file       = " << stream_file << std::endl; 
+    std::cerr << "batch_size_arg    = " << batch_size_arg << std::endl;
+    std::cerr << "height_factor_arg = " << height_factor_arg << std::endl;
+  } else {
+    stream_file = argv[1];
+    batch_size_arg = atoi(argv[2]);
+    height_factor_arg = atof(argv[3]);
+  }
 
   testing::InitGoogleTest(&argc, argv);
   int ret = RUN_ALL_TESTS(); 
   MPI_Finalize();
   return ret; 
 }
+
--- test/test_runner.cpp
+++ test/test_runner.cpp
@@ -2,8 +2,10 @@
 #include <gtest/gtest.h>
 #include "util.h"
 
-
-std::string stream_file;
+static const std::string fname = __FILE__;
+static size_t pos = fname.find_last_of("\\/");
+static const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
+std::string stream_file = curr_dir + "/test_streams/multiples_graph_1024_stream.data";
 
 int main(int argc, char** argv) {
   if (argc > 1)
